fd5e925112af334ad1d5bdb0f633fa7057ac3e0d
Suman Dangol <sdangol368@gmail.com>
2025-11-08 08:49:10 -0600

    insert sample data in all tables

--- MovieRentalDatabase/movie_rental_db.sql
+++ MovieRentalDatabase/movie_rental_db.sql
@@ -36,3 +36,61 @@ CREATE TABLE Rentals(
     rental_date DATE DEFAULT CURRENT_DATE,
     return_date DATE
 );
+
+
+
+--Part 2: Insert Sample Data
+
+INSERT INTO Movies (title, director, release_year, available, runtime_minutes, language, genre)
+VALUES
+('Oppenheimer', 'Christopher Nolan', 2023, FALSE, 180, 'English', 'Biography'),
+('Train to Busan', 'Yeon Sang-ho', 2016, TRUE, 118, 'Korean', 'Horror'),
+('Spirited Away', 'Hayao Miyazaki', 2001, FALSE, 125, 'Japanese', 'Animation'),
+('Dune: Part Two', 'Denis Villeneuve', 2024, FALSE, 166, 'English', 'Sci-Fi'),
+('The Dark Knight', 'Christopher Nolan', 2008, FALSE, 152, 'English', 'Action'),
+('Titanic', 'James Cameron', 1997, TRUE, 195, 'English', 'Romance'),
+('3 Idiots', 'Rajkumar Hirani', 2009, FALSE, 170, 'Hindi', 'Comedy'),
+('Hero', 'Zhang Yimou', 2002, TRUE, 99, 'Chinese', 'Action'),
+('Roma', 'Alfonso Cuarón', 2018, FALSE, 135, 'Spanish', 'Drama'),
+('Parasite', 'Bong Joon Ho', 2019, TRUE, 132, 'Korean', 'Thriller'),
+('Your Name', 'Makoto Shinkai', 2016, TRUE, 112, 'Japanese', 'Romance'),
+('Crouching Tiger, Hidden Dragon', 'Ang Lee', 2000, FALSE, 120, 'Chinese', 'Martial Arts'),
+('Zindagi Na Milegi Dobara', 'Zoya Akhtar', 2011, TRUE, 155, 'Hindi', 'Adventure'),
+('The Wandering Earth', 'Frant Gwo', 2019, TRUE, 125, 'Chinese', 'Sci-Fi');
+
+
+INSERT INTO Members (name, email, phone)
+VALUES
+('Suman Dangol', 'suman.dangol@example.com', '603-555-1001'),
+('Bobby Reed', 'bobby.reed@example.com', '603-555-1002'),
+('Shamir Khandaker', 'shamir246@example.com', '603-555-1003'),
+('Jeff Maxwell', 'jeff_max@example.com', '603-555-1004'),
+('Michael Scott', 'michael.scott@example.com', '603-555-1005'),
+('Pam Beesly', 'pam.beesly@example.com', '603-555-1006'),
+('Jim Halpert', 'jim.halpert@example.com', '603-555-1007'),
+('Dwight Maxwell', 'dwight.maxwell@example.com', '603-555-1008'),
+('Harry Potter', 'harry.potter@example.com', '603-555-1009'),
+('Hermione Granger', 'hermione.granger@example.com', '603-555-1010');
+
+
+INSERT INTO Rentals (movie_id, member_id, rental_date, return_date)
+VALUES
+(10, 1, '2025-10-01', '2025-10-05'),
+(1, 2, '2025-10-02', NULL),
+(12, 3, '2025-10-02', '2025-10-07'),
+(2, 4, '2025-10-02', NULL),
+(3, 5, '2025-10-05', '2025-10-09'),
+(4, 1, '2025-10-05', '2025-10-10'),
+(5, 2, '2025-10-06', '2025-10-11'),
+(5, 3, '2025-10-08', NULL),
+(1, 1, '2025-10-09', NULL),
+(1, 4, '2025-10-09', '2025-10-13'),
+(8, 5, '2025-10-10', NULL),
+(9, 6, '2025-10-11', '2025-10-15'),
+(1, 7, '2025-10-11', '2025-10-16'),
+(11, 8, '2025-10-11', NULL),
+(2, 9, '2025-10-14', '2025-10-18'),
+(1, 10, '2025-10-15', NULL),
+(1, 9, '2025-10-15', NULL),
+(2, 2, '2025-10-16', '2025-10-22'),
+(6,4 , '2025-10-18', NULL);
